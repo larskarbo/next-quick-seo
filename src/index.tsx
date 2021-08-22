adaf298e49de1231a4ea5c602e11c15cfb98a61f
Lars Karbo <mail@larskarbo.no>
2021-08-22 21:59:49 +0200

    go

---
+++ src/index.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import Head from 'next/head'
+
+export default function QuickSeo({
+  title,
+  description,
+  image
+}: {
+  title: string
+  description?: string
+  image?: string
+}) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property='og:title' content={title} />
+      <meta name='twitter:title' content={title} />
+      <meta name='title' content={title} />
+
+      {description && (
+        <>
+          <meta name='description' content={description} />
+          <meta property='og:description' content={description} />
+          <meta name='twitter:description' content={description} />
+        </>
+      )}
+
+      {image && (
+        <>
+          <meta property='og:image' content={image} />
+          <meta name='twitter:image' content={image} />
+          <meta name='twitter:card' content='summary_large_image' />
+        </>
+      )}
+    </Head>
+  )
+}
